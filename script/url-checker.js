29e0eaf0e675d5b3f5cab94c25e5d0f49eb64819
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-10-07 21:25:06 -0300

    fix: corrigindo json

--- script/url-checker.js
+++ script/url-checker.js
@@ -1,42 +1,4 @@
-#!/bin/env node
-/** 
- * 
- * MIT License
- * 
- * Copyright (c) 2024 Bassem Dghaidi
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- * ---
- * 
- * This code is for illustration purposes. Do not use it in production.
- * 
- * This script will crawl all the pages of this website and call each external URL
- * to make sure none return a 404 error. These are markdown pages.
- * 
- * Usage:
- *  node url-checker.js -t <timeout> -p <path>
- * 
- * Options:
- *  -t, --timeout <timeout>  The timeout in seconds for each request. Default is 5000.
- */
-
+// oi
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
@@ -58,7 +20,6 @@ const args = parser.parse_args();
 const timeoutSeconds = args.timeout * 1000 || 5000;
 const postsDir = args.dir || __dirname + '/../src/posts';
 
-// Recusively get all the markdown files (.md) only in the posts directory
 const walkSync = (dir, filelist = []) => {
     fs.readdirSync(dir).forEach(file => {
         const filePath = path.join(dir, file);
